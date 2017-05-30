12d69dd5c8a8c2ce110d99f16218ec3853904dba
Vitaliy Balash <vitalik.balash@gmail.com>
2017-05-30 20:02:11 +0300

    update drawing algorithm

--- src/nodes/qsg_circular_segment_node.cpp
+++ src/nodes/qsg_circular_segment_node.cpp
@@ -2,7 +2,6 @@
 
 #include <QSGFlatColorMaterial>
 #include <QSGGeometry>
-#include <cmath>
 
 #include "qsg_smooth_color_material.h"
 #include "../tools/calc.h"
@@ -11,7 +10,7 @@ QSGCircularSegmentNode::QSGCircularSegmentNode()
     : mGeometry(new QSGGeometry(QSGGeometry::defaultAttributes_Point2D(), 0, 0, QSGGeometry::UnsignedIntType)),
       mMaterial(new QSGFlatColorMaterial)
 {
-    mGeometry->setDrawingMode(QSGGeometry::DrawTriangleFan);
+    mGeometry->setDrawingMode(QSGGeometry::DrawTriangleStrip);
     setGeometry(mGeometry);
     setFlag(QSGNode::OwnsGeometry);
 
@@ -26,19 +25,36 @@ void QSGCircularSegmentNode::update(QPointF center, float radius, float startAnd
     markDirty(QSGNode::DirtyMaterial);
 
     // update points
-    int vCount = Calc::vertexContInCircleSegment(radius, startAndgle, endAngle);
-    mGeometry->allocate(vCount);
+    int sCount = Calc::segmentContInCircleSegment(radius, startAndgle, endAngle);
+    int vCount = 2 + sCount;
+    int iCount = vCount + std::floor(sCount / 2.0);
+
+    mGeometry->allocate(vCount, iCount);
     QSGGeometry::Point2D *vertices = mGeometry->vertexDataAsPoint2D();
 
+    // VERTICES
     vertices[0].set(center.x(), center.y());
-
-    double step = (endAngle - startAndgle) / (vCount - 2);
-    for (int i = 0; i < vCount - 1; ++i)
+    double step = (endAngle - startAndgle) / sCount;
+    for (int i = 0; i < vCount; ++i)
     {
         double angle = startAndgle + step * i;
         double x = center.x() + radius * cos(angle);
         double y = center.y() + radius * sin(angle);
         vertices[i + 1].set(x, y);
+    }
+
+    // INDEXES
+    uint *indexes = mGeometry->indexDataAsUInt();
+    for (int i = 0; i < iCount; ++i)
+    {
+        if (i % 3 == 0)
+        {
+            indexes[i] = 0;
+        }
+        else
+        {
+            indexes[i] = i - std::floor((double)i / 3.0);
+        }
     }
 
     markDirty(QSGNode::DirtyGeometry);
