574f23f145e8c0a6ad8cc72ae851c2564b11d812
Vitaliy Balash <vitalik.balash@gmail.com>
2017-06-03 22:30:13 +0300

    add antialiasing

--- src/nodes/qsg_circular_segment_node.cpp
+++ src/nodes/qsg_circular_segment_node.cpp
@@ -21,28 +21,44 @@ QSGCircularSegmentNode::QSGCircularSegmentNode()
 void QSGCircularSegmentNode::update(QPointF center, float radius, float startAndgle, float endAngle, int lineWidth, QColor color, bool antialiasing)
 {
     Color4ub fillColor = colorToColor4ub(color);
+    Color4ub transparent = {0, 0, 0, 0};
 
-    int sCount = Calc::segmentContInCircleSegment(radius, startAndgle, endAngle);
-    int vCount = 2 + sCount;
-    int iCount = vCount + std::floor(sCount / 2.0);
+    int sumVerticesCount = 0;
+    int sumIndexesCount = 0;
 
-    mGeometry->allocate(vCount, iCount);
+    int segmentsCount = Calc::segmentContInCircleSegment(radius, startAndgle, endAngle);
+    int verticesCount = 2 + segmentsCount;
+    sumVerticesCount += verticesCount;
+
+    int outerIndexesCount = verticesCount + std::floor(segmentsCount / 2.0);
+    sumIndexesCount += outerIndexesCount;
+
+    int outerAntialiasedIndexesCount = 0;
+    if (antialiasing)
+    {
+        sumVerticesCount += verticesCount;
+        outerAntialiasedIndexesCount = 2 * (segmentsCount + 1) + 5;
+        sumIndexesCount += outerAntialiasedIndexesCount;
+    }
+
+    // allocate memory
+    mGeometry->allocate(sumVerticesCount, sumIndexesCount);
+
+    // draw outer triangles
     SmoothVertex *vertices = reinterpret_cast<SmoothVertex *>(mGeometry->vertexData());
-
-    // VERTICES
     vertices[0].set(center.x(), center.y(), fillColor, 0, 0);
-    double step = (endAngle - startAndgle) / sCount;
-    for (int i = 0; i < vCount; ++i)
+    double step = (endAngle - startAndgle) / segmentsCount;
+    for (int i = 0; i < verticesCount - 1; ++i)
     {
         double angle = startAndgle + step * i;
         double x = center.x() + radius * cos(angle);
         double y = center.y() + radius * sin(angle);
+
         vertices[i + 1].set(x, y, fillColor, 0, 0);
     }
 
-    // INDEXES
     uint *indexes = mGeometry->indexDataAsUInt();
-    for (int i = 0; i < iCount; ++i)
+    for (int i = 0; i < outerIndexesCount; ++i)
     {
         if (i % 3 == 0)
         {
@@ -52,6 +68,43 @@ void QSGCircularSegmentNode::update(QPointF center, float radius, float startAnd
         {
             indexes[i] = i - std::floor((double)i / 3.0);
         }
+    }
+
+    // draw outer fade triangles
+    if (antialiasing)
+    {
+        // vertices
+        double dAngle = 0.5 * (startAndgle + endAngle);
+        double dx = 2 * std::cos(dAngle);
+        double dy = 2 * std::sin(dAngle);
+
+        vertices[0].dx = dx;
+        vertices[0].dy = dy;
+        vertices[verticesCount].set(vertices[0].x, vertices[0].y, transparent, -dx, -dy);
+
+        for (int i = 1; i < verticesCount; ++i)
+        {
+            double angle = startAndgle + step * i;
+            double dAngle = angle + M_PI_2 * step * i / (endAngle - startAndgle) - M_PI_4;
+            double dx = std::cos(dAngle);
+            double dy = std::sin(dAngle);
+
+            vertices[i].dx = -dx;
+            vertices[i].dy = -dy;
+            vertices[verticesCount + i].set(vertices[i].x, vertices[i].y, transparent, dx, dy);
+        }
+
+        // indexes
+        indexes[outerIndexesCount] = 0;
+        indexes[outerIndexesCount + 1] = 0;
+        indexes[outerIndexesCount + 2] = verticesCount;
+        for (int i = 1; i < verticesCount; ++i)
+        {
+            indexes[outerIndexesCount + 2 * i + 1] = i;
+            indexes[outerIndexesCount + 2 * i + 2] = verticesCount + i;
+        }
+        indexes[outerIndexesCount + 2 * verticesCount + 1] = 0;
+        indexes[outerIndexesCount + 2 * verticesCount + 2] = verticesCount;
     }
 
     markDirty(QSGNode::DirtyGeometry);
